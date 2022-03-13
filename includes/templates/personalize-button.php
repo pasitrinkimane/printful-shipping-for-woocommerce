d2644cc6f35724e36651af690e69744c23d09a9b
Vytautas Dirzauskas <vytautas@kilo.lt>
2022-03-13 16:48:12 +0200

    fixes

--- includes/templates/personalize-button.php
+++ includes/templates/personalize-button.php
@@ -5,8 +5,13 @@
  * @var string $pfc_button_text
  */
 ?>
-<a class="button"
-        style="background-color: <?php esc_attr_e($pfc_button_color); ?>"
-        onclick="Printful_Product_Customizer.onCustomizeClick( '<?php echo esc_url($site_url); ?>')">
-    <?php esc_html_e($pfc_button_text, 'printful'); ?>
-</a>
+<div class="personalize-btn-wrapper">
+    <p>
+        <?php esc_html_e($pfc_modal_title, 'printful'); ?>
+    </p>
+    <a class="btn btn-secondary"
+       style="background-color: <?php esc_attr_e($pfc_button_color); ?>"
+       onclick="Printful_Product_Customizer.onCustomizeClick( '<?php echo esc_url($site_url); ?>')">
+        <?php esc_html_e($pfc_button_text, 'printful'); ?>
+    </a>
+</div>
